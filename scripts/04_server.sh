75d927a372656166a782a7bfb3b59fac2ee842bc
ysnt27 <ysnt27@users.noreply.github.com>
2019-08-28 14:37:45 +0900

    port number 8888

--- scripts/04_server.sh
+++ scripts/04_server.sh
@@ -4,4 +4,4 @@ source $HOME/adc2019/venv36/bin/activate
 export GOOGLE_APPLICATION_CREDENTIALS=$HOME/adc2019/server/keyfile.json
 $(gcloud beta emulators datastore --data-dir $HOME/adc2019/work/datastore env-init)
 cd $HOME/adc2019/server/
-gunicorn -b :4280 --access-logfile '-' main:app
+gunicorn -b :8888 --access-logfile '-' main:app
